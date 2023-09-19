e66a2a1e3638d7f4284c90e7542f3f244653bbb2
Breno Gomes da Fonseca <brenogfonseca@gmail.com>
2023-09-19 11:54:46 -0300

    componente register

---
+++ src/components/register/index.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { useForm, SubmitHandler } from "react-hook-form";
+import FormBuilder from "../form/FormBuilder";
+
+export type Inputs = {
+  cpf: string;
+  nome: string;
+  matricula: string;
+  data: string;
+  telefone: string;
+  email: string;
+  funcao: string;
+  cargo: string;
+  curso_gestor: string;
+  data_entrada: string;
+  data_inicio_docencia: string;
+  obs_curso_gestor: string;
+};
+
+export default function Register() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
+  return <FormBuilder control={control} errors={errors} />;
+
+  // return (
+  //   <form style={{ marginTop: "20px" }} onSubmit={handleSubmit(onSubmit)}>
+  //     <Grid container spacing={1}>
+  //       <Grid item xs={3}>
+  //         <CpfInput cpfDTO={cpfDTO} control={control} errors={errors} />
+  //       </Grid>
+  //       <Grid item xs={3}>
+  //         <TextInput
+  //           control={control}
+  //           name="matricula"
+  //           label="Matricula"
+  //           errors={errors}
+  //         />
+  //       </Grid>
+  //       <Grid item xs={3}>
+  //         <TextInput
+  //           control={control}
+  //           name="nome"
+  //           label="Nome"
+  //           errors={errors}
+  //         />
+  //       </Grid>
+  //       <Grid item xs={3}>
+  //         <FoneInput
+  //           control={control}
+  //           name="telefone"
+  //           label="Telefone"
+  //           errors={errors}
+  //         />
+  //       </Grid>
+  //       <Grid item xs={3}>
+  //         <EmailInput
+  //           control={control}
+  //           name="email"
+  //           label="Email"
+  //           errors={errors}
+  //         />
+  //       </Grid>
+  //       <Grid item xs={3}>
+  //         <SelectInput
+  //           control={control}
+  //           name="funcao"
+  //           label="Função"
+  //           errors={errors}
+  //         />
+  //       </Grid>
+  //       <Grid item xs={3}>
+  //         <Button variant={"contained"} type="submit">
+  //           enviar
+  //         </Button>
+  //       </Grid>
+  //     </Grid>
+  //   </form>
+  // );
+}
