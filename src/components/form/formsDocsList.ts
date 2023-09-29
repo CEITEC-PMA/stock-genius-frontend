51f284c216f8a3fd809824d109fda6da719df87c
Raphael Cassimiro <raphaelcmelob.b@gmail.com>
2023-09-29 14:19:17 -0300

    SIS-9-docslist step envio de docs candidato

---
+++ src/components/form/formsDocsList.ts
@@ -0,0 +1,78 @@
+export const documentsList = [
+  {
+    id: 1,
+    name: "Requerimento de registro do candidato(a)/indicado(a) a Gestor(a)",
+  },
+  {
+    id: 2,
+    name: "Ficha de qualificação do(a) candidato(a)/indicado(a)",
+  },
+  {
+    id: 3,
+    name: "Cópia do título de habilitação profissional autenticada em cartório",
+  },
+  {
+    id: 4,
+    name: "Cópia da Proposta de Trabalho, em consonância com o Projeto Político Pedagógico da Unidade de Ensino",
+  },
+  {
+    id: 5,
+    name: "Declaração de disponibilidade para dedicação exclusiva",
+  },
+  {
+    id: 6,
+    name: "Cópia do certificado do curso de Formação Continuada para Gestores, realizado no biênio 2022-2023, devidamente autenticado em cartório ou Declaração do CEFOPE comprovando a aprovação na formação",
+  },
+  {
+    id: 7,
+    name: "Declaração de efetivo exercício na Rede Municipal de Ensino",
+  },
+  {
+    id: 8,
+    name: "Certificado do curso de Formação Básica para candidatos/indicados a gestores para o biênio 2024/2025 oferecido pela SEMED",
+  },
+  {
+    id: 9,
+    name: "Certidão Negativa Criminal e Cível Estadual",
+  },
+  {
+    id: 10,
+    name: "Certidão Negativa Criminal e Cível da Justiça Federal",
+  },
+  {
+    id: 11,
+    name: "Certidão Negativa Criminal da Justiça Eleitoral",
+  },
+  {
+    id: 12,
+    name: "Original da Pesquisa emitida pela Câmara de Dirigentes Lojistas de Anápolis – CDL",
+  },
+  {
+    id: 13,
+    name: "Declaração de não interrupção do mandato por licenças opcionais",
+  },
+  {
+    id: 14,
+    name: "Declaração de tempo de docência",
+  },
+  {
+    id: 15,
+    name: "Declaração de tempo de modulação na Unidade de Ensino",
+  },
+  {
+    id: 16,
+    name: "Declaração de que possui apenas uma matrícula na Rede Municipal de Educação",
+  },
+  {
+    id: 17,
+    name: "Declaração de nada consta",
+  },
+  {
+    id: 18,
+    name: "Declaração de regularidade com o FUMMDE",
+  },
+  {
+    id: 19,
+    name: "Laudo para professores readaptados expedido pela perícia médica (somente candidatos(as) ou indicados(as) readaptados(as))",
+  },
+];
