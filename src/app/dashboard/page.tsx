e8d6c650d90a98a89e7fa9510c0dcfafff3248a6
Cleidimar Dias <cleidimarcdsc@gmail.com>
2023-10-06 17:00:55 -0300

    SED-18-Configurado Dasboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,42 @@
+'use client'
+import { Box, Card, Typography, CardMedia, CardContent, CardActions, Button, Paper, useMediaQuery, useTheme } from "@mui/material";
+import { Sriracha } from "next/font/google";
+
 export default function DashboardPage() {
-  return <div>Dashboard</div>;
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'))
+  return (
+    <>
+      <Box display='flex' flexDirection="column" justifyContent='space-around' height='100vh' >
+        <Box flex={1}>
+          <Typography variant={smDown ? 'h6' : mdDown ? 'h5' : 'h4'} textAlign="center" marginTop={2} color=" #0f4c81" >
+            ELEIÇÕES MUNICIPAIS DE DIRETORES BIÊNIO 2024/25
+          </Typography>
+        </Box>
+
+        <Box
+          borderRadius="10px"
+          flex={3}
+          // height={250}
+          justifyContent="center" marginX={6} paddingY={3} alignItems="center"
+          sx={{
+            backgroundImage: "url('/girl.jpeg')",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center ",
+            backgroundSize: "cover",
+            height: '380px',
+          }}
+        >
+        </Box>
+        <Box marginX={2} paddingY={3} display="flex" alignItems='center' flex={1}>
+          <Typography textAlign='center' mt={3} variant="body1" color=" #0f4c81">
+            Bem-vindo à nossa plataforma de eleição de diretores escolares, uma ferramenta crucial para moldar o futuro da educação em nossa comunidade. Aqui, reconhecemos a importância fundamental das eleições de diretores para aprimorar e fortalecer nossa comunidade escolar.
+          </Typography>
+        </Box>
+      </Box >
+
+    </>
+
+  )
 }
