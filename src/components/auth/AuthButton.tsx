b45741747e804a1592bf9da0965f5d67672c4e6a
Breno Gomes da Fonseca <brenogfonseca@gmail.com>
2023-10-06 11:23:21 -0300

    auth button

--- src/components/auth/AuthButton.tsx
+++ src/components/auth/AuthButton.tsx
@@ -1,20 +1,31 @@
+import { Box, Grid, IconButton, Typography } from "@mui/material";
 import { signIn, signOut, useSession } from "next-auth/react";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 export default function AuthButton() {
   const { data: session } = useSession();
-
-  if (session) {
-    return (
-      <>
-        {session?.user?.name}
-        <button onClick={() => signOut()}>Sign Out</button>
-      </>
-    );
-  }
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <Grid height={"100%"} item container justifyContent={"end"}>
+      <Grid item alignSelf={"center"}>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="inherit"
+          noWrap
+          sx={{ flexGrow: 1, textAlign: "right" }}
+        >
+          USER_NAME
+        </Typography>
+      </Grid>
+      <Grid item alignSelf={"center"}>
+        <IconButton
+          onClick={() => signOut()}
+          color="inherit"
+          sx={{ marginLeft: "10px" }}
+        >
+          <LogoutIcon />
+        </IconButton>
+      </Grid>
+    </Grid>
   );
 }
