48f93c3e27ca5f179be36d0be944ad4695cc64c3
Cleidimar Dias <cleidimarcdsc@gmail.com>
2023-10-06 11:02:28 -0300

    configurado apiUrl

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -18,8 +18,32 @@ import { useRouter } from "next/navigation";
 import Header from "../dashboard/Header";
 import AppBarComponent from "../dashboard/AppBarComponent";
 import { apiUrl } from "@/utils/api";
+import { TransitionProps } from "@mui/material/transitions";
+import { Alert, Fade, IconButton, Snackbar, Stack } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function LoginPage() {
+
+  const [open, setOpen] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState("")
+  const [errorInep, setErrorInep] = React.useState("")
+
+  // const handleClick = () => {
+  //   setOpen(true);
+  // };
+
+  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+
+
+
+
+
   const dispatch = useDispatch();
   const router = useRouter();
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -30,6 +54,10 @@ export default function LoginPage() {
       password: data.get("senha"),
     };
     const { password, inep } = dataToSend;
+    if (!inep) {
+      setErrorInep("Campo Obrigatório")
+      alert("Por favor digite o número do INEP")
+    }
     if (!password) {
       alert("Por favor digite uma senha");
     } else {
@@ -46,13 +74,19 @@ export default function LoginPage() {
           body: JSON.stringify(dataToSend),
         })
           .then(async (response) => {
-            const resJson = await response.json();
-            const token = resJson.usuario.token;
-            localStorage.setItem("token", token);
-            router.push("/dashboard");
-          })
-          .catch((error) => {
-            console.log(error);
+            if (response.status === 200) {
+              const resJson = await response.json();
+              const token = resJson.usuario.token;
+              localStorage.setItem("token", token);
+              router.push("/dashboard");
+
+            } else if (response.status === 401) {
+              throw new Error("Inep ou Senha Inválidos")
+            }
+
+          }).catch((error) => {
+            setErrorMessage(error.message);
+            setOpen(true)
           });
         // dispatch(userSlice.actions.loginUser(dataToSend));
         // router.push("/dashboard");
@@ -61,7 +95,17 @@ export default function LoginPage() {
   };
 
   return (
+
     <Container maxWidth="xs">
+
+      <Stack spacing={2} sx={{ width: '100%' }}>
+
+        <Snackbar anchorOrigin={{ vertical: "top", horizontal: "left" }} open={open} autoHideDuration={8000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+            {errorMessage}
+          </Alert>
+        </Snackbar>
+      </Stack>
       <CssBaseline />
       <Box
         sx={{
@@ -102,8 +146,7 @@ export default function LoginPage() {
             type="submit"
             fullWidth
             variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
+            sx={{ mt: 3, mb: 2 }}          >
             Enviar
           </Button>
         </Box>
