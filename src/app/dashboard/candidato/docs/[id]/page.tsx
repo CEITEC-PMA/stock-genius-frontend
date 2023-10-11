a3502d8cccdb7c534387ceaef1f120a39c176b19
Raphael Cassimiro <raphaelcmelob.b@gmail.com>
2023-10-11 08:57:45 -0300

    SED-24-docs nomes dinamicos, post docs, favicon

--- src/app/dashboard/candidato/docs/[id]/page.tsx
+++ src/app/dashboard/candidato/docs/[id]/page.tsx
@@ -1,57 +1,119 @@
 "use client";
 import DocslistCard from "@/components/form/DocsListCard";
 import { documentsList } from "@/components/form/formsDocsList";
-import { Box, Button, Container, Paper, Typography } from "@mui/material";
-import React from "react";
+import { useUserContext } from "@/userContext";
+import { Box, Container, Paper, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { apiUrl } from "@/utils/api";
+
+interface Candidato {
+  candidato: CandidatoClass;
+}
+
+interface CandidatoClass {
+  curso_gestor: string;
+  foto: any[];
+  docs: { [key: string]: Doc };
+  deletado: boolean;
+  _id: string;
+  cpf: string;
+  nome: string;
+  email: string;
+  telefone: string;
+  cargo: string;
+  funcao: string;
+  matricula: number;
+  data_entrada_inst: Date;
+  data_entrada_docencia: Date;
+  tempo_modulacao: string;
+  tempo_docencia: string;
+  protocolo: string;
+  zona: string;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+}
+
+interface Doc {
+  file: string;
+  original_file: string;
+}
 
 const onSubmit = (data: any) => {
   console.log(data);
   console.log("enviou");
 };
 
-const nomeUnidade = "Escola Municipal Teste";
-const nomeCandidato = "Candidato Teste da Silva";
-
-export default function FormDocs() {
+export default function DocsCandidato({ params }: { params: { id: string } }) {
   const { control, handleSubmit } = useForm();
+  const { user, setUser } = useUserContext();
+  const [candidato, setCandidato] = useState<CandidatoClass>();
+  const router = useRouter();
+
+  useEffect(() => {
+    const getDadosCandidato = async (id: string, token: string) => {
+      const response = await fetch(
+        `${apiUrl}/api/v1/candidato/candidatoId/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const responseJson = await response.json();
+      setCandidato(responseJson.candidato);
+      return;
+    };
+    const token = localStorage.getItem("token");
+    if (token) {
+      getDadosCandidato(params.id, token);
+    }
+  }, [params.id]);
 
   return (
-    <Container>
-      <Typography variant="h4" textAlign="center">
-        Documentos do candidato
-      </Typography>
-      <Paper sx={{ padding: "12px" }}>
-        <Box margin="0 12px">
-          <Typography variant="h6">
-            Unidade de ensino:{" "}
-            <span style={{ fontWeight: "normal", fontSize: "1rem" }}>
-              {nomeUnidade}
-            </span>
-          </Typography>
-          <Typography variant="h6">
-            Nome do candidato(a)/indicado(a):{" "}
-            <span style={{ fontWeight: "normal", fontSize: "1rem" }}>
-              {nomeCandidato}
-            </span>
-          </Typography>
-        </Box>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {documentsList.map((document, i) => (
-            <DocslistCard name={document.name} key={i} />
-          ))}
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <Button
-              type="submit"
-              variant="contained"
-              style={{ margin: "12px" }}
-              fullWidth
-            >
-              Salvar
-            </Button>
+    <Box margin="24px">
+      <Container>
+        <Typography variant="h4" marginBottom="12px" textAlign="center">
+          Documentos do candidato
+        </Typography>
+        <Paper sx={{ padding: "12px" }}>
+          <Box margin="0 12px">
+            <Typography variant="h6">
+              Unidade de ensino:{" "}
+              <span style={{ fontWeight: "normal", fontSize: "1rem" }}>
+                {user?.nome}
+              </span>
+            </Typography>
+            <Typography variant="h6">
+              Nome do candidato(a)/indicado(a):{" "}
+              <span style={{ fontWeight: "normal", fontSize: "1rem" }}>
+                {candidato?.nome}
+              </span>
+            </Typography>
           </Box>
-        </form>
-      </Paper>
-    </Container>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {candidato ? (
+              documentsList.map((document, i) => (
+                <DocslistCard
+                  candidato={candidato}
+                  name={document.name}
+                  categoria={document.categoria}
+                  key={i}
+                />
+              ))
+            ) : (
+              <div>Carregando...</div>
+            )}
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            ></Box>
+          </form>
+        </Paper>
+      </Container>
+    </Box>
   );
 }
--- src/components/form/DocsListCard.tsx
+++ src/components/form/DocsListCard.tsx
@@ -6,9 +6,40 @@ import {
   Typography,
   useMediaQuery,
 } from "@mui/material";
-import React from "react";
+import React, { ChangeEvent } from "react";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { styled } from "@mui/material/styles";
+import { useRouter } from "next/navigation";
+import { apiUrl } from "@/utils/api";
+
+interface CandidatoClass {
+  curso_gestor: string;
+  foto: any[];
+  docs: { [key: string]: Doc };
+  deletado: boolean;
+  _id: string;
+  cpf: string;
+  nome: string;
+  email: string;
+  telefone: string;
+  cargo: string;
+  funcao: string;
+  matricula: number;
+  data_entrada_inst: Date;
+  data_entrada_docencia: Date;
+  tempo_modulacao: string;
+  tempo_docencia: string;
+  protocolo: string;
+  zona: string;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+}
+
+interface Doc {
+  file: string;
+  original_file: string;
+}
 
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -22,8 +53,37 @@ const VisuallyHiddenInput = styled("input")({
   width: 1,
 });
 
-export default function DocslistCard({ name }: { name: string }) {
+export default function DocslistCard({
+  name,
+  categoria,
+  candidato,
+}: {
+  name: string;
+  categoria: string;
+  candidato: CandidatoClass;
+}) {
   const isSmallScreen = useMediaQuery("(max-width: 600px)");
+  const router = useRouter();
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const fileLoaded = e.target.files[0];
+      const formData = new FormData();
+      formData.append("file", fileLoaded);
+      fetch(
+        `${apiUrl}/api/v1/candidato/docs/${candidato._id}?cpf=${candidato.cpf}&categoria=${categoria}`,
+        {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          method: "PUT",
+          body: formData,
+        }
+      ).then(() => {
+        router.refresh();
+      });
+    }
+  };
 
   return (
     <Grid item xs={12} sm={6} md={4} lg={3}>
@@ -46,7 +106,10 @@ export default function DocslistCard({ name }: { name: string }) {
               sx={{ whiteSpace: "nowrap" }}
             >
               Escolher arquivo
-              <VisuallyHiddenInput type="file" />
+              <VisuallyHiddenInput
+                onChange={(e) => handleOnChange(e)}
+                type="file"
+              />
             </Button>
           </Box>
         </Box>
