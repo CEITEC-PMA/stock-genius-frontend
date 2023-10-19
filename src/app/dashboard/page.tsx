74c21c8ba0856d471d652fe0c47f4b8c624404ad
Cleidimar Dias <cleidimarcdsc@gmail.com>
2023-10-19 14:41:46 -0300

    SED-18-corrigido imgaem

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -29,23 +29,7 @@ export default function DashboardPage() {
             }}
             alt="Estudante" />
         </Box>
-        {/* <Box component={Paper}
-          borderRadius="10px"
-          flex={3}
-          // height={250}
-          justifyContent="center" marginX={6} paddingY={3} alignItems="center"
 
-          sx={{
-            //backgroundImage: "url('/girl.jpeg')",
-            backgroundImage: `url(${girl})`,
-            //backgroundImage: "url('/girl.jpeg')",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center ",
-            backgroundSize: "cover",
-            height: '380px',
-          }}
-        > 
-        </Box> */}
         <Box marginX={2} paddingY={3} display="flex" alignItems='center' flex={1}>
           <Typography textAlign='center' mt={3} variant="body1" color=" #0f4c81">
             Bem-vindo à nossa plataforma de eleição de diretores escolares, uma ferramenta crucial para moldar o futuro da educação em nossa comunidade. Aqui, reconhecemos a importância fundamental das eleições de diretores para aprimorar e fortalecer nossa comunidade escolar.
