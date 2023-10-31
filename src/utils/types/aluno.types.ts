2275812f541936a4132bfd78279200ebda0e9a68
Cleidimar Dias <cleidimarcdsc@gmail.com>
2023-10-31 10:25:13 -0300

    criado types para alunos

---
+++ src/utils/types/aluno.types.ts
@@ -0,0 +1,20 @@
+export interface Aluno {
+  aluno_votou: boolean;
+  fotoresp_votou: boolean;
+  votante: boolean;
+  deletado: boolean;
+  _id: string;
+  cpf: string;
+  nome: string;
+  mae: string;
+  pai: string;
+  responsavel: string;
+  serie: string;
+  turma: string;
+  zona: string;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+}
+
+
