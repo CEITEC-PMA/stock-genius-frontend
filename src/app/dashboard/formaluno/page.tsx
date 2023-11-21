0775021f4fe6c1e5126e2ddc163cf3c6bd5a1928
Raphael Cassimiro <raphaelcmelob.b@gmail.com>
2023-11-20 22:21:16 -0300

    SED-55-adiciona adicionar aluno

--- src/app/dashboard/formaluno/page.tsx
+++ src/app/dashboard/formaluno/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import {
   TextField,
   Button,
@@ -9,15 +9,102 @@ import {
   Box,
   useTheme,
   useMediaQuery,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+  InputLabel,
 } from "@mui/material";
 import { useUserContext } from "@/userContext";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { apiUrl } from "@/utils/api";
+import { useRouter } from "next/navigation";
+
+interface FormData {
+  nome: string;
+  responsavel1: string;
+  responsavel2: string | null;
+  responsavel3: string | null;
+  serie: string;
+}
 
 const FormularioCadastro = () => {
   const { user } = useUserContext();
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down("sm"));
   const mdDown = useMediaQuery(theme.breakpoints.down("md"));
+  const router = useRouter();
+
+  const serieOptions = [
+    "ED. INF. I",
+    "ED. INF. II",
+    "ED. INF. III",
+    "ED. INF. IV",
+    "ED. INF. V",
+    "1° ANO",
+    "2° ANO",
+    "3° ANO",
+    "4° ANO",
+    "5° ANO",
+    "6° ANO",
+    "7° ANO",
+    "8° ANO",
+  ];
+
+  const [formData, setFormData] = useState<FormData>({
+    nome: "",
+    responsavel1: "",
+    responsavel2: null,
+    responsavel3: null,
+    serie: "",
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    const paraMaiuscula = value.toUpperCase();
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: paraMaiuscula,
+    }));
+  };
+
+  const handleSelectChange = (e: SelectChangeEvent<string>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    const token = localStorage.getItem("token");
+    const filteredFormData = Object.fromEntries(
+      Object.entries(formData).filter(
+        ([_, value]) => value !== null && value !== ""
+      )
+    );
+    try {
+      const response = await fetch(`${apiUrl}/api/v1/aluno/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(filteredFormData),
+      });
+
+      if (response.ok) {
+        console.log("Dados enviados com sucesso!");
+        alert("Cadastro concluído com sucesso!");
+        router.push("/dashboard/alunos");
+      } else {
+        console.error("Falha ao enviar os dados.");
+        alert("Não foi possível cadastrar o aluno, tente novamente");
+      }
+    } catch (error) {
+      alert("Não foi possível cadastrar o aluno, tente novamente");
+      console.error("Erro ao enviar os dados:", error);
+    }
+  };
 
   return (
     <Box margin="0" padding="0" height={`calc(100vh - 66px)`} overflow="hidden">
@@ -47,7 +134,11 @@ const FormularioCadastro = () => {
                 label="Nome do Aluno"
                 variant="outlined"
                 fullWidth
-                required // torna o campo obrigatório
+                required
+                sx={{ backgroundColor: "#fff" }}
+                name="nome"
+                value={formData.nome}
+                onChange={handleInputChange}
               />
             </Grid>
             <Grid item xs={12}>
@@ -55,21 +146,52 @@ const FormularioCadastro = () => {
                 label="Responsável 1"
                 variant="outlined"
                 fullWidth
-                required // torna o campo obrigatório
+                required
+                sx={{ backgroundColor: "#fff" }}
+                name="responsavel1"
+                value={formData.responsavel1}
+                onChange={handleInputChange}
               />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField label="Responsável 2" variant="outlined" fullWidth />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField label="Responsável 3" variant="outlined" fullWidth />
             </Grid>
             <Grid item xs={12}>
               <TextField
-                label="Série"
+                label="Responsável 2"
                 variant="outlined"
-                required // torna o campo obrigatório
+                fullWidth
+                sx={{ backgroundColor: "#fff" }}
+                name="responsavel2"
+                value={formData.responsavel2}
+                onChange={handleInputChange}
               />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Responsável 3"
+                variant="outlined"
+                fullWidth
+                sx={{ backgroundColor: "#fff" }}
+                name="responsavel3"
+                value={formData.responsavel3}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <InputLabel>Série</InputLabel>
+              <Select
+                variant="outlined"
+                placeholder="Selecione"
+                required
+                sx={{ width: 180, backgroundColor: "#fff" }}
+                name="serie"
+                value={formData.serie}
+                onChange={handleSelectChange}
+              >
+                {serieOptions.map((option) => (
+                  <MenuItem key={option} value={option}>
+                    {option}
+                  </MenuItem>
+                ))}
+              </Select>
             </Grid>
             <Grid item xs={12}>
               <Button
@@ -77,6 +199,7 @@ const FormularioCadastro = () => {
                 size="large"
                 fullWidth
                 startIcon={<CheckCircleIcon fontSize="large" />}
+                onClick={handleSubmit}
               >
                 Salvar
               </Button>
